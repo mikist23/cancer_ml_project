ad90740b444f515c6765a400fa22309480e0b492
@Mikist <mikeochango3@gmail.com>
2024-08-13 23:42:43 +0300

    add sidebar section

--- app/main.py
+++ app/main.py
@@ -3,6 +3,12 @@ import pickle
 import pandas as pd
 
 
+# sidebar section
+
+def add_sidebar():
+    st.sidebar.header('Cell Nuclei Detals')
+
+# main function
 def main():
     st.set_page_config(
         page_title = 'Breast Cancer Predictor',
@@ -11,8 +17,21 @@ def main():
         initial_sidebar_state= 'expanded'
     )
 
-    st.write('Hello woorlds')
+    # sidebar
 
+    add_sidebar()
+    # container
+
+    with st.container():
+        st.title('Breast Cancer Predictor')
+        st.write('Machine-learning dashboard to predict whether a cell cluster is benign or malignant using Python and Streamlit. machine-learning dashboard to predict whether a cell cluster is benign or malignant using Python and Streamlit.')
+
+    col1, col2 = st.columns([4,1]) 
+
+    with col1:
+        st.write('This is col 1')  
+    with col2:
+        st.write('This is col 2')  
 
 
 if __name__ == '__main__':
