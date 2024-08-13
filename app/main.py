fa0760cfb7fdc6d14938d3dbd403f7f462b342c8
@Mikist <mikeochango3@gmail.com>
2024-08-13 23:56:31 +0300

    create a slider for each label

--- app/main.py
+++ app/main.py
@@ -2,11 +2,61 @@ import streamlit as st
 import pickle
 import pandas as pd
 
+# cleaned data
+
+def get_clean_data():
+    data = pd.read_csv('data.csv')
+    data = data.drop(['Unnamed: 32', 'id'], axis = 1)
+    data['diagnosis'] = data['diagnosis'].map({'M':1,"B":0})
+    return data
+
 
 # sidebar section
-
 def add_sidebar():
     st.sidebar.header('Cell Nuclei Detals')
+
+    data = get_clean_data()
+
+      # Define the labels
+    slider_labels = [
+        ("Radius (mean)", "radius_mean"),
+        ("Texture (mean)", "texture_mean"),
+        ("Perimeter (mean)", "perimeter_mean"),
+        ("Area (mean)", "area_mean"),
+        ("Smoothness (mean)", "smoothness_mean"),
+        ("Compactness (mean)", "compactness_mean"),
+        ("Concavity (mean)", "concavity_mean"),
+        ("Concave points (mean)", "concave points_mean"),
+        ("Symmetry (mean)", "symmetry_mean"),
+        ("Fractal dimension (mean)", "fractal_dimension_mean"),
+        ("Radius (se)", "radius_se"),
+        ("Texture (se)", "texture_se"),
+        ("Perimeter (se)", "perimeter_se"),
+        ("Area (se)", "area_se"),
+        ("Smoothness (se)", "smoothness_se"),
+        ("Compactness (se)", "compactness_se"),
+        ("Concavity (se)", "concavity_se"),
+        ("Concave points (se)", "concave points_se"),
+        ("Symmetry (se)", "symmetry_se"),
+        ("Fractal dimension (se)", "fractal_dimension_se"),
+        ("Radius (worst)", "radius_worst"),
+        ("Texture (worst)", "texture_worst"),
+        ("Perimeter (worst)", "perimeter_worst"),
+        ("Area (worst)", "area_worst"),
+        ("Smoothness (worst)", "smoothness_worst"),
+        ("Compactness (worst)", "compactness_worst"),
+        ("Concavity (worst)", "concavity_worst"),
+        ("Concave points (worst)", "concave points_worst"),
+        ("Symmetry (worst)", "symmetry_worst"),
+        ("Fractal dimension (worst)", "fractal_dimension_worst"),
+    ]
+
+    for label, key in slider_labels:
+        st.sidebar.slider(
+            label,
+            min_value = float(0),
+            max_value = float(data[key].max())
+        )
 
 # main function
 def main():
